03cdb5279c81f15f3ef57c3d68872ea783c2127d
realrelay <157940529+realrelay@users.noreply.github.com>
2024-01-28 05:27:25 +0000

    Update killerice.py
    Added: Choices, Cleaner code, Different host
    Fixed: Reboot command, Function calling

--- killerice.py
+++ killerice.py
@@ -1,18 +1,38 @@
-import subprocess
-import os
+import subprocess, time, os
 
-def ping_google():
+def main_choice():
+    mainchoice = input("Do you want to use the default settings? y/n: ")
+    if mainchoice == "y":
+        tmeout = 10
+        rebsec = 0
+        print("Settings Set - Starting")
+    elif mainchoice == "n":
+        tmeout = int(input("Ping timeout time: "))
+        rebsec = int(input("Reboot Time: "))
+
+        print("Settings Set - Starting")
+    return tmeout, rebsec
+
+def ping_google(tmeout, rebsec):
     try:
-        result = subprocess.run(['ping', 'google.com', '-c', '4'], capture_output=True, text=True, timeout=10)
+        result = subprocess.run(['ping', '8.8.8.8', '-c', '4'], capture_output=True,
+                                text=True, timeout=tmeout)
         if result.returncode == 0:
             print("Ping successful!")
         else:
-            print("Ping failed.")
-            os.system('reboot')
-    except subprocess.TimeoutExpired:
-        print("Timeout: Ping took too long.")
+            print(f"Ping failed - Rebooting in {rebsec} seconds")
+            time.sleep(rebsec)
+            os.system('systemctl reboot')
 
-        # Add an infinite loop
-        while True:
-            ping_google()
-            time.sleep(60)  # Wait for 60 seconds before the next ping attempt
+    except subprocess.TimeoutExpired:
+        print(f"Ping timed out after {tmeout} seconds - Rebooting in {rebsec} seconds")
+        time.sleep(rebsec)
+        os.system('systemctl reboot')
+
+    while True:
+        ping_google(tmeout, rebsec)
+        time.sleep(60)
+
+if __name__ == "__main__":
+    tmeout, rebsec = main_choice()
+    ping_google(tmeout, rebsec)
